c67e8fe2e7b9bd4be5dd6a6504fe8e09555ca849
Ramanan S <rams2@publicisgroupe.net>
2021-12-28 20:01:32 +0530

    event bus added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser());
@@ -14,14 +15,23 @@ app.get("/posts/:id/comments", (req, res) => {
   res.status(200).send(comments);
 });
 
-app.post("/posts/:id/comment", (req, res) => {
+app.post("/posts/:id/comment", async (req, res) => {
   const { comment } = req.body;
   const commentid = randomBytes(6).toString("hex");
   const dataMap = { id: commentid, comment };
   const comments = commentsBypostId[req.params.id] || [];
   comments.push(dataMap);
   commentsBypostId[req.params.id] = comments;
+  // emit event from here to event bus service.
+  await axios.post("http://localhost:6000/events", {
+    type: "COMMENT_CREATED",
+    payload: { ...dataMap, postId: req.params.id },
+  });
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(5000, () => {
